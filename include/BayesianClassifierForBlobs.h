8d7c834d80b70dbaeab60be2c3be09cb6497ace5
leofaber89 <leonardo.baroncelli26@gmail.com>
2017-07-28 10:08:08 +0200

    Corretti parametri delle distribuzioni delle features per la classificazione bayesiana

--- include/BayesianClassifierForBlobs.h
+++ include/BayesianClassifierForBlobs.h
@@ -47,21 +47,21 @@ class BayesianClassifierForBlobs
         float computeProbabilityFromDistribution(float x,normal_distribution<double> distribution);
 
 
-        normal_distribution<double> bgPixelMeanDistribution = normal_distribution<double>( 5.25778, 0.336926);
-        normal_distribution<double> fluxPixelMeanDistribution = normal_distribution<double>( 11.8688, 5.23051);
-        normal_distribution<double> bgAreaDistribution = normal_distribution<double>( 16.9377, 10.1723);
-        normal_distribution<double> fluxAreaDistribution = normal_distribution<double>( 97.3049, 37.9206);
-        normal_distribution<double> bgPhotonsInBlobDistribution = normal_distribution<double>( 1.53307, 0.954141);
-        normal_distribution<double> fluxPhotonsInBlobDistribution = normal_distribution<double>( 10.3087, 6.39049);
-        normal_distribution<double> bgPhotonsClosenessDistribution = normal_distribution<double>( 1.28323, 0.908676);
-        normal_distribution<double> fluxPhotonsClosenessDistribution = normal_distribution<double>( 2.7578, 0.972047);
+        normal_distribution<double> bgPixelMeanDistribution = normal_distribution<double>( 1.19936, 0.06062);
+        normal_distribution<double> fluxPixelMeanDistribution = normal_distribution<double>( 1.67467, 0.368148);
+        normal_distribution<double> bgAreaDistribution = normal_distribution<double>( 131.769, 36.872);
+        normal_distribution<double> fluxAreaDistribution = normal_distribution<double>( 303.736, 111.971);
+        normal_distribution<double> bgPhotonsInBlobDistribution = normal_distribution<double>( 1.00452, 0.322571);
+        normal_distribution<double> fluxPhotonsInBlobDistribution = normal_distribution<double>( 2.96629, 1.37758);
+        normal_distribution<double> bgPhotonsClosenessDistribution = normal_distribution<double>( 0.555733, 1.32262);
+        normal_distribution<double> fluxPhotonsClosenessDistribution = normal_distribution<double>( 3.83731, 2.48066);
 
         /// ADD DISTRIBUTION IF NEEDED
 
-        float countBgBlobs = 514;
-        float countFluxBlobs = 528;
-        float bgFrequency = 0.493282;
-        float fluxFrequency = 0.506718;
+        float countBgBlobs = 221;
+        float countFluxBlobs = 178;
+        float bgFrequency = 0.553885;
+        float fluxFrequency = 0.446115;
 
 
 
