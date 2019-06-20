d460e6f2934fa958b2ed61d8f2cef003a16e0da5
Leonardo Baroncelli <leonardo.baroncelli@inaf.it>
2019-06-20 14:18:29 +0200

    updated script for running the blobs_list_generator

--- run_blobs_list_generator.sh
+++ run_blobs_list_generator.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 module load agile-B25-r5
 module load opencv-2.4.13.6
+module load healpix-3.31
 
 export PFILES=$PWD/conf
 
@@ -8,4 +9,4 @@ export PFILES=$PWD/conf
 #make agextspot
 
 
-./bin/AG_extspot_v2_blobs_list_generator /home/rt/baroncelli.leonardo/maps_for_agextspot blobs_list.json no 0.5 0.5 7.5 yes
+./bin/AG_extspot_v2_blobs_list_generator /home/rt/baroncelli.leonardo/maps_for_agextspot agile 0.5 0.5 7.5 yes agile_0.5_0.5_7.5.json
