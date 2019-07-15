d88f47524a6ff83e18e2278984e2182f3a1a65f5
LeonardoBaroncelli <leonardo.baroncelli@inaf.it>
2019-07-15 11:19:02 +0200

    Modified run script with new inpur param

--- utility_scripts/run_blobs_list_generator.sh
+++ utility_scripts/run_blobs_list_generator.sh
@@ -5,18 +5,18 @@ module load opencv-2.4.13.6
 #module load healpix-3.31
 module load healpix-3.50
 
-if [ "$#" -ne 1 ]; then
-  echo "> Please provide the map format: 'agile' or 'healpix'"
+if [ "$#" -ne 2 ]; then
+  printf "Please provide:\n> the map format: 'agile' or 'healpix'\n> the path to the input photon lists folder\n"
   return
 fi
 
 echo "Map format: $1"
-
+echo "Input folder: $2"
 
 export PFILES=$PWD/conf
 
 if [ "$1" = "healpix" ]; then
-  ./bin/AG_extspot_v2_blobs_list_generator blobs json_blobs_data /home/zollino/AGILE_REAL_IMAGE healpix 0.5 0.5 7.5 no yes
+  ./bin/AG_extspot_v2_blobs_list_generator blobs json_blobs_data $2 healpix 0.5 0.5 7.5 no yes
   # ./bin/AG_extspot_v2_blobs_list_generator blobs json_blobs_data /home/zollino/AGILE_REAL_IMAGE healpix 0.25 0.25 7.5 no yes
 fi
 if [ "$1" = "agile" ]; then
